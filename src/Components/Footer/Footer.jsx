29e3f940121ccf7ae71b985ef64ac068ca548ff0
MariumAkterDev <mariumshorna8@gmail.com>
2025-02-13 21:16:47 +0600

    update commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,61 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import footerImg from "../../assets/images/footerImg.png";
+import { CommonBtn } from '../CommonButton/CommonBtn'
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <section>
+      <div className="MainFooter">
+        <div className="container">
+          {/* ---------------------------- Row 1 --------------------------------- */}
+          <div className="footerRow1">
+            <div className='footerRow1Child' >
+            <img src={footerImg} alt="Ring" />
+            <div className="footerRow1Txt">
+              <h2 className="Title">Stay in touch?</h2>
+              <p>
+                Subscribe to get special offers, free giveaways, and
+                once-in-a-lifetime deals.
+              </p>
+              <CommonBtn btnContent={'Subscribe'} btnColors={'bg-brand-Color text-[#fff]'}/>
+            </div>
+            </div>
+          </div>
+          {/* --------------------------------- Row 2 -------------------------------------- */}
+          <div className="footerRow2">
 
-export default Footer
+            <div className="footerRow2SingCard">
+              <h4>Product</h4>
+              <div className='footerMenu ' >
+                <Link to=''>Privacy Policy</Link>
+                <Link to=''>Terms of Service</Link>
+                <Link to=''>FAQ</Link>
+              </div>
+            </div>
+
+            <div className="footerRow2SingCard">
+              <h4>Resources</h4>
+              <div className='footerMenu ' >
+                <Link to=''>Documentation</Link>
+                <Link to=''>Case Studies</Link>
+              </div>
+            </div>
+
+            <div className="footerRow2SingCard">
+              <h4>Company</h4>
+              <div className='footerMenu ' >
+                <Link to=''>About Us</Link>
+                <Link to=''>Contact Us</Link>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
--- src/Layouts/LayoutOne.jsx
+++ src/Layouts/LayoutOne.jsx
@@ -8,7 +8,7 @@ const LayoutOne = () => {
     <>
         <Nav/>
         <Outlet/>
-        {/* <Footer/> */}
+        <Footer/>
     </>
   )
 }
